c7a498d87d16b1f8eebd83aadda55d89ded1a39a
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2024-02-02 16:39:56 +0300

    transform webpack config to typescript

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {sum} from "./test";
+console.log('Webpack template working?');
+console.log(sum(5,5));
---
+++ webpack.config.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+type Mode = 'production' | 'development';
+interface EnvVariables {
+    mode: Mode
+}
+export default (env:EnvVariables) => {
+    const config: webpack.Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+            new webpack.ProgressPlugin(),
+        ]
+    }
+    return config;
+}
