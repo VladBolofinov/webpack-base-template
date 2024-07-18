b52275cd1d4be7d129aad3532156d63c50e71aca
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2024-07-18 13:20:05 +0300

    refactor: delete unused import

--- src/providers/StoreProvider/StoreProvider.tsx
+++ src/providers/StoreProvider/StoreProvider.tsx
@@ -2,7 +2,6 @@ import { type ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from '@/providers/StoreProvider/config/store';
 import { type StateSchema } from '@/providers/StoreProvider/config/StateSchema';
-import { useNavigate } from 'react-router-dom';
 
 interface StoreProviderProps {
     children?: ReactNode;
